a8688b30862a29de3b4f9a8b9ffef559d9868075
Zhenghao Fang <f97zhh@gmail.com>
2021-04-26 04:17:49 -0400

    postRequest

--- src/main/java/com/example/demo/servers/servelet.java
+++ src/main/java/com/example/demo/servers/servelet.java
@@ -1,14 +1,12 @@
 package com.example.demo.servers;
-
+import java.util.*;
 import com.example.demo.entities.precint;
 import com.example.demo.handler.StateHandler;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
-
+@CrossOrigin
 @RestController
 @RequestMapping(path = "api/v1/test1")
 public class servelet {
@@ -24,5 +22,18 @@ public class servelet {
     public List<precint> getPrecint()
     {
         return stateHandler.getPrecint();
+    }
+    @RequestMapping("/index")
+    String index(){
+        //mapped to hostname:port/home/index/
+        return "Hello from index";
+    }
+    @PostMapping()
+    public void addMember(@RequestBody String state) {
+//        String[] arrOfStr = state.split("\\+");
+//
+//        for (String a : arrOfStr)
+//            System.out.println(a+"iiii");
+        System.out.println(state);
     }
 }
