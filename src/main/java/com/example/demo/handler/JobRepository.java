86ce656b00ecbb58b88d2358f2e76076a75004b7
Xinhan <huangxinhan99@gmail.com>
2021-04-29 17:20:06 -0400

    job repo

---
+++ src/main/java/com/example/demo/handler/JobRepository.java
@@ -0,0 +1,9 @@
+package com.example.demo.handler;
+
+import com.example.demo.entities.Job;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface JobRepository extends JpaRepository<Job, String> {
+}
