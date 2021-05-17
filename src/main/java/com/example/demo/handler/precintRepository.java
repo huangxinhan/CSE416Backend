f817ac3ceea99f0bd4ec38fbf828f4d9387e4e7d
Zzzzzzino <56735427+Zzzzzzino@users.noreply.github.com>
2021-05-17 19:20:41 +0800

    update

--- src/main/java/com/example/demo/handler/precintRepository.java
+++ src/main/java/com/example/demo/handler/precintRepository.java
@@ -5,7 +5,11 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.util.Streamable;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface precintRepository extends JpaRepository<Precinct,String> {
     Streamable<Precinct> findByprecinctIDContaining(String firstname);
+
+    List<Precinct> findByprecinctIDStartsWith(String haha);
 }
